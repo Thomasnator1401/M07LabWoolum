32fa61400a1c1b5bd354e84a71d3d2f43c85a70d
Thomasnator1401 <145989955+Thomasnator1401@users.noreply.github.com>
2023-12-10 20:36:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-$(document).ready(function() {
-    $('#contactForm').submit(function(e) {
-        e.preventDefault();
-        alert('Thank you for reaching out! We will get back to you soon.');
+// A function to check if the user's email and password match the database
+function validateLogin(email, password) {
+  // Fetch the user data from the database using the email
+  let user = db.collection("users").doc(email).get();
+  // If the user exists, compare the password with the stored hash
+  if (user) {
+    let hash = user.data().password;
+    // Use bcrypt to compare the password and the hash
+    bcrypt.compare(password, hash, function(err, result) {
+      // If the result is true, the password is correct
+      if (result) {
+        // Return the user object
+        return user;
+      } else {
+        // Return an error message
+        return "Invalid password";
+      }
     });
-});
+  } else {
+    // Return an error message
+    return "User not found";
+  }
+}
