323e837458669b9e7501760a3dd8e82a8f8b207f
Thomasnator1401 <145989955+Thomasnator1401@users.noreply.github.com>
2023-12-10 21:37:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,82 @@
-// A function to check if the user's email and password match the database
-function validateLogin(email, password) {
-  // Fetch the user data from the database using the email
-  let user = db.collection("users").doc(email).get();
-  // If the user exists, compare the password with the stored hash
-  if (user) {
-    let hash = user.data().password;
-    // Use bcrypt to compare the password and the hash
-    bcrypt.compare(password, hash, function(err, result) {
-      // If the result is true, the password is correct
-      if (result) {
-        // Return the user object
-        return user;
-      } else {
-        // Return an error message
-        return "Invalid password";
-      }
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyD-...",
+  authDomain: "my-login-app.firebaseapp.com",
+  projectId: "my-login-app",
+  storageBucket: "my-login-app.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdefg"
+};
+firebase.initializeApp(firebaseConfig);
+
+// Get references to the elements in the HTML
+var txtEmail = document.getElementById('txtEmail');
+var txtPassword = document.getElementById('txtPassword');
+var btnLogin = document.getElementById('btnLogin');
+var btnSignUp = document.getElementById('btnSignUp');
+var btnLogout = document.getElementById('btnLogout');
+
+// Add event listeners for the buttons
+btnLogin.addEventListener('click', e => {
+  // Get the email and password from the input fields
+  var email = txtEmail.value;
+  var password = txtPassword.value;
+  // Sign in the user with Firebase
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(user => {
+      // User signed in successfully
+      console.log('User signed in');
+    })
+    .catch(error => {
+      // Handle errors
+      console.log(error.code, error.message);
     });
+});
+
+btnSignUp.addEventListener('click', e => {
+  // Get the email and password from the input fields
+  var email = txtEmail.value;
+  var password = txtPassword.value;
+  // Create a new user with Firebase
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(user => {
+      // User created successfully
+      console.log('User created');
+    })
+    .catch(error => {
+      // Handle errors
+      console.log(error.code, error.message);
+    });
+});
+
+btnLogout.addEventListener('click', e => {
+  // Sign out the user with Firebase
+  firebase.auth().signOut()
+    .then(() => {
+      // User signed out successfully
+      console.log('User signed out');
+    })
+    .catch(error => {
+      // Handle errors
+      console.log(error.code, error.message);
+    });
+});
+
+// Add an observer for the auth state
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    // User is signed in
+    console.log('User is signed in:', user.email);
+    // Show the logout button and hide the login and signup buttons
+    btnLogout.style.display = 'block';
+    btnLogin.style.display = 'none';
+    btnSignUp.style.display = 'none';
   } else {
-    // Return an error message
-    return "User not found";
+    // User is signed out
+    console.log('User is signed out');
+    // Hide the logout button and show the login and signup buttons
+    btnLogout.style.display = 'none';
+    btnLogin.style.display = 'block';
+    btnSignUp.style.display = 'block';
   }
-}
+});
